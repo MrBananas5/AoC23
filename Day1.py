c7fcfa0f281a6f6bbe868378d40bf702cdcbe5b7
MrBananas5 <117031730+MrBananas5@users.noreply.github.com>
2023-12-04 17:21:26 +0000

    Update Day1.py
    added comments, removed an unused function

--- Day1.py
+++ Day1.py
@@ -1,31 +1,25 @@
-inp = open("input.txt","r").readlines()
+inp = open("input1.txt","r").readlines()
 n = 0
 digs = ["one","two","three","four","five","six","seven","eight","nine"]
 
-digsDict = {"oneight":"18","threeight":"38","fiveight":"58","sevenine":"79","eightwo":"82","nineight":"98","sevenineight":"798","eighthree":"83","twone":"21","twoneight":"218"}
-def isint(inp): return (inp.upper() == inp.lower() and not (inp == "\n"))
-
-def findin (toFind, stri):
-    newline = ""
-    foundlen = 0
-    stri
-    print(stri)
+digsDict = {"oneight":"18","threeight":"38","fiveight":"58","sevenine":"79","eightwo":"82","nineight":"98","sevenineight":"798","eighthree":"83","twone":"21","twoneight":"218"} #overlaps (one eight = oneight)
+def isint(inp): return (inp.upper() == inp.lower() and not (inp == "\n"))#forgor .isdigit() exists
 
 for i in inp:
-    newline = i
+    newline = i # preserves the original input in case i wanted it later
     print(i)
-    for j in digsDict.keys():
-        newline = newline.replace(j, digsDict[j])
-    for j in digs:
-        newline = newline.replace(j, str(digs.index(j)+1))
+    for j in digsDict.keys(): #for each overlapping number string
+        newline = newline.replace(j, digsDict[j]) #replace with number
+    for j in digs: # for each normal number string
+        newline = newline.replace(j, str(digs.index(j)+1)) # replace with its index +1 (its value)
     print(newline)
     
     first = ""
-    for c in newline:
+    for c in newline: 
         if isint(c): first = c
     last = ""
-    for c in newline[::-1]:
+    for c in newline[::-1]: #looks through backwards for numbers
         if isint(c): last = c
-    print(int(last+first))
+    print(int(last+first))# lotsa prints sorry
     n+= int(last + first)
 print(n)    
