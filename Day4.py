284d2dcee23c05da31953120b874ea39ee48b36f
MrBananas5 <117031730+MrBananas5@users.noreply.github.com>
2023-12-04 17:31:22 +0000

    Update Day4.py
    added comments

--- Day4.py
+++ Day4.py
@@ -1,18 +1,18 @@
 inp = open("input4.txt","r").readlines()
-def remove(l,c):
+def remove(l,c): #returns a (list) copy of iterable l, with no instances of c
     toRet = []
     for i in l:
         if not i == c:toRet.append(i)
     return toRet
-class ticket():
+class ticket(): #ticket class
     def __init__(self,text):
-        self.text = text.replace("\n","")
-        self.numbers = list(self.text)
+        self.text = text.replace("\n","")# removes EoL character
+        self.numbers = list(self.text) 
         self.numbers = self.text.split(":")[1]
-        self.id = int(self.text.split(":")[0].replace("Card ",""))
-        self.winners = [int(i) for i in remove(self.numbers.split("|")[0].split(" "),"")]
-        self.numbers = [int(i) for i in remove(self.numbers.split("|")[1].split(" "),"")]
-        self.matches = []
+        self.id = int(self.text.split(":")[0].replace("Card ","")) # splits it by colon, and removes 'Card ' - to get the ID of the ticket
+        self.winners = [int(i) for i in remove(self.numbers.split("|")[0].split(" "),"")] #the numbers on the left (separated by ' ') - removes empty parts too
+        self.numbers = [int(i) for i in remove(self.numbers.split("|")[1].split(" "),"")] #the numbers on the right (separated by ' ')
+        self.matches = [] #prepares matches 
         
         self.countWinners()
         
@@ -20,7 +20,7 @@ class ticket():
         #print(self.matches)
         #print(self.points,"\n")
 
-    def countWinners(self):
+    def countWinners(self):#counts the points and stores the matches for later use
         n = 0
         for i in self.winners:
             for c in self.numbers:
@@ -33,16 +33,16 @@ class ticket():
 
 tickets = [ticket(i) for i in inp]
 
-copies = []
+copies = [] # stores the numbers of each ticket
 n = 0
 for i in tickets:
-    copies.append(i.id)
-    k = 0
-    for j in copies:
-        if j == i.id:
+    copies.append(i.id) #there exists at least one copy of each ticket
+    k = 0 #used to get the total number of each ticket
+    for j in copies: #looks at every copy of every ticket
+        if j == i.id: 
             k+=1
             n+=1
-            for c in range(1,1+len(i.matches)):copies.append(i.id+c)
+            for c in range(1,1+len(i.matches)):copies.append(i.id+c)#stores the number of every ticket it needs to 
     print(i.id,len(i.matches),k,n)
 
 
